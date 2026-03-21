c7ab212916193d1389eaa3321271564bde509046
Scitechgen1 <155811314+ktg-charoenchai@users.noreply.github.com>
2026-03-21 14:52:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function change(el) {
     el.classList.toggle('change')
 }
+
+document.getElementById("change").onclick = function() {
+    window.location.href = "https://ktg-charoenchai.github.io/BWS-Sci-tech-gen-1-student-Home/";
+}
